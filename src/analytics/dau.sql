883703a4b4269aeeee002f2a9acb0d0098e23d7f
Millena Pereira <pfernandesmillena@gmail.com>
2026-06-19 13:09:22 -0300

    adiciona pipeline para executar life_cycle corrige SQL

--- src/analytics/dau.sql
+++ src/analytics/dau.sql
@@ -1,6 +1,9 @@
-SELECT substr(DtCriacao,0, 11) AS DtDia,
-       count(DISTINCT IdCliente) AS DAU
+-- DAU - DAILY ACTIVE USERS
 
+SELECT  substr(DtCriacao, 0, 11) AS DTdia,
+        count (DISTINCT idCliente) AS DAU
 FROM transacoes
 GROUP BY 1
 ORDER BY DtDia
+
+
--- src/analytics/mau.sql
+++ src/analytics/mau.sql
@@ -1,32 +1,30 @@
-WITH tb_daily AS (
 
-    SELECT DISTINCT 
-        date(substr(DtCriacao,0,11)) AS DtDia,
+WITH tb_daily AS(
+    
+    SELECT DISTINCT
+        substr(DtCriacao, 0, 11) AS DtDia,
         IdCliente
 
     FROM transacoes
-    order by DtDia
-
+    ORDER BY DtDia 
+    
 ),
 
-tb_distinct_day AS (
+tb_distinct_day AS(
 
-    SELECT
-            DISTINCT DtDia AS dtRef
+    SELECT DISTINCT DtDia AS dtRef 
+
     FROM tb_daily
 
 )
 
-SELECT t1.dtRef,
-       count( distinct IdCliente) AS MAU,
-       count(distinct t2.dtDia) AS qtdeDias
+SELECT t1.dtRef, count(distinct IdCliente) AS MAU
 
 FROM tb_distinct_day AS t1
 
 LEFT JOIN tb_daily AS t2
-ON t2.DtDia <= t1.dtRef
+On t2.DtDia <= t1.dtRef
 AND julianday(t1.dtRef) - julianday(t2.DtDia) < 28
-
 GROUP BY t1.dtRef
 
-ORDER BY t1.dtRef ASC
+order BY t1.dtRef asc
